036d1c6a762a6ceddd49844013e7427d242fe140
Thakki07 <thakkithakki13@gmail.com>
2025-04-14 17:50:06 +0530

    first commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './Footer.css'
-import footer_log from '../../assets/footer_logo.svg'
+import profile_img from '../../assets/profile_img.png'
 import instagram_icon from '../../assets/instagram_icon.webp'
 function Footer() {
   return (
     <div className="footer">
         <div className="footer-top">
             <div className="footer-top-left">
-                    <img src={footer_log} alt="" />
+                    <img src={profile_img} alt="Logo" style={{ height: '40px', objectFit: 'contain',}} />
                     <p>I am a front-end developer from Mangalore, Karnataka, with experience in building and delivering several projects.</p>
             </div>
             <div className="footer-top-right">
-                <div className="footer-subscribe"> <a href="https://www.instagram.com/thakki_07/" style={{textDecoration:'none',color:'white'}}><p>Follow Us On</p></a><img src= {instagram_icon} alt="" /> </div>
+                <div className="footer-subscribe"> <a href="https://www.instagram.com/thakki_07/" style={{textDecoration:'none',color:'white'}}><p>Follow Me On</p></a><img src= {instagram_icon} alt="" /> </div>
             </div>
         </div>
         <hr />
